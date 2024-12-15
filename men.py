b7d3306ef8912c45c4b728d2a549073f9d436741
lina <af702820@gmail.com>
2024-12-15 18:52:38 +0600

    Потоки

--- men.py
+++ men.py
@@ -1,40 +1,45 @@
 from task1 import task1
 from task2 import task2
 from task3 import task3
-from concurrent.futures import ThreadPoolExecutor
+import threading
 
 def print_menu():
     """Выводит меню с доступными заданиями."""
     print("\nМеню:")
-    print("1. Задание 1")
-    print("2. Задание 2")
-    print("3. Задание 3")
+    print("1. Ввод исходных данных")
+    print("2. Выполнение алгоритма")
+    print("3. Вывод результата")
     print("4. Завершение работы программы")
 
 def main():
     """Основная функция программы, которая управляет выбором задания пользователем."""
-    with ThreadPoolExecutor() as executor:
-        while True:
-            print_menu()  # Выводим меню
-            choice = input("Выберите пункт меню: ")  # Запрашиваем выбор пользователя
+    while True:
+        print_menu()  # Выводим меню
+        choice = input("Выберите пункт меню: ")  # Запрашиваем выбор пользователя
 
-            if choice == '1':
-                executor.submit(task1)  # Запускаем задание 1 в фоновом режиме
-                executor.join()
-            elif choice == '2':
-                executor.submit(task2)  # Запускаем задание 2 в фоновом режиме
-                executor.join()
-            elif choice == '3':
-                executor.submit(task3)  # Запускаем задание 3 в фоновом режиме
-                executor.join()
-            elif choice == '4':
-                print("Завершение работы программы.")  # Сообщаем о завершении
-            executor.join()
+        if choice == '1':
+            thread = threading.Thread(target=task1)
+            thread.start()  # Запускаем задание 1 в новом потоке
+            thread.join()   # Ожидаем завершения потока
+
+        elif choice == '2':
+            thread = threading.Thread(target=task2)
+            thread.start()  # Запускаем задание 2 в новом потоке
+            thread.join()   # Ожидаем завершения потока
+
+        elif choice == '3':
+            thread = threading.Thread(target=task3)
+            thread.start()  # Запускаем задание 3 в новом потоке
+            thread.join()   # Ожидаем завершения потока
+
+        elif choice == '4':
+            print("Завершение работы программы.")  # Сообщаем о завершении
             break  # Выходим из цикла
 
         else:
-            
-         print("Неверный выбор.")  # Обработка неверного выбора
+            print("Неверный выбор.")  # Обработка неверного выбора
+        
+        input("Нажмите Enter, чтобы продолжить...")  # Пауза перед повторным выводом меню
 
 if __name__ == "__main__":
     main()  # Запуск основной функции при выполнении скрипта
